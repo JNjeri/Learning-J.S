488e57725a9ea080be8cb849d236f1a7b7bb4f40
Joyce Njeri <tamiranjeri98@gmail.com>
2018-02-17 12:40:37 +0300

    added file

---
+++ IfElse.js
@@ -0,0 +1,9 @@
+var issunday = false;
+
+if(issunday){
+	console.log("watch movies")
+
+}
+	else{
+		console.log("learn code")
+	}
---
+++ variables.js
@@ -0,0 +1,6 @@
+var class_size = 23;
+var isweekday = false;
+var classname ="Akirachix";
+var showsize = 6.7;
+//this is used to print
+console.log(class_size)
